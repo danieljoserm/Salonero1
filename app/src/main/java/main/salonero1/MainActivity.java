e23aa4203c4db11ee283355d14bd1803af2ffabb
daniel <danieljoserm@gmail.com>
2016-11-03 08:42:23 -0600

    funciona navi bar con animacion y los tabs

--- app/src/main/java/main/salonero1/MainActivity.java
+++ app/src/main/java/main/salonero1/MainActivity.java
@@ -35,14 +35,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -87,6 +80,8 @@ public class MainActivity extends AppCompatActivity
 
         // Setting the ViewPager For the SlidingTabsLayout
         tabs.setViewPager(pager);
+
+
 
 
 
