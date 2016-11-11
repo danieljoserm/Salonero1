48d2fa878265fc13a689f8e0ff72bbd8de2044d0
daniel <danieljoserm@gmail.com>
2016-11-11 17:14:46 -0600

    probando items

--- app/src/main/java/main/salonero1/Tabs/Tab1.java
+++ app/src/main/java/main/salonero1/Tabs/Tab1.java
@@ -3,16 +3,19 @@ package main.salonero1.Tabs;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import main.salonero1.Adapters.adaptermenuitem;
 import main.salonero1.R;
+import main.salonero1.clases.menuitem;
 
 
 /**
@@ -20,6 +23,10 @@ import main.salonero1.R;
  */
 public class Tab1 extends Fragment {
 String texto;
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter Adapter;
+    List<menuitem> menuitems;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,6 +36,72 @@ String texto;
 
         TextView prueba = (TextView) v.findViewById(R.id.Textviewfragmento1);
         prueba.setText(texto);
+
+
+
+        menuitems= new ArrayList<>();
+        menuitem hola= new menuitem();
+        hola.setnombremenuitem("hola");
+        hola.setCantidad(1);
+        hola.setprecio(2);
+
+
+        menuitem hola2= new menuitem();
+        hola2.setnombremenuitem("si");
+        hola2.setCantidad(2);
+        hola2.setprecio(234);
+
+        menuitem hola3= new menuitem();
+        hola3.setnombremenuitem("hola");
+        hola3.setCantidad(424);
+        hola3.setprecio(235);
+
+        menuitem hola4= new menuitem();
+        hola4.setnombremenuitem("adios");
+        hola4.setCantidad(256);
+        hola4.setprecio(2343);
+
+
+        menuitem hola5= new menuitem();
+        hola5.setnombremenuitem("adios");
+        hola5.setCantidad(256);
+        hola5.setprecio(2343);
+
+        menuitem hola6= new menuitem();
+        hola6.setnombremenuitem("adios");
+        hola6.setCantidad(256);
+        hola6.setprecio(2343);
+        menuitem hola7= new menuitem();
+        hola7.setnombremenuitem("adios");
+        hola7.setCantidad(256);
+        hola7.setprecio(2343);
+
+
+        menuitems.add(hola);
+        menuitems.add(hola2);
+        menuitems.add(hola3);
+        menuitems.add(hola4);
+        menuitems.add(hola5);
+        menuitems.add(hola6);
+        menuitems.add(hola7);
+
+
+        mRecyclerView = (RecyclerView)v.findViewById(R.id.recyclerview_menuitem);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(v.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //  mLayoutManager = new GridLayoutManager(rootView.getContext(), 1);
+        // mRecyclerView.setLayoutManager(mLayoutManager);
+
+        Adapter = new adaptermenuitem(menuitems);
+        //
+        mRecyclerView.setAdapter(Adapter);
+
+
+
+
 
 
         return v;
