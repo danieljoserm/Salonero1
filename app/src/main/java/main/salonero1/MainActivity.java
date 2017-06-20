e61acb67ab0699dfde3dce7adc65e85daccf8808
danie <danieljoserm@gmail.com>
2017-06-20 10:55:27 -0600

    prueba github

--- app/src/main/java/main/salonero1/MainActivity.java
+++ app/src/main/java/main/salonero1/MainActivity.java
@@ -1,6 +1,6 @@
 package main.salonero1;
 
-
+//prueba github
 import android.app.FragmentManager;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -508,15 +508,15 @@ else{return false;
         }
 
 
-        SharedPreferences prefs =
-                getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+     //   SharedPreferences prefs =
+       //         getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
 
-        SharedPreferences.Editor editor = prefs.edit();
+       // SharedPreferences.Editor editor = prefs.edit();
 
 
-        editor.putString("email", "modificado@email.com");
-        editor.putString("nombre", "Prueba");
-        editor.commit();
+        //editor.putString("email", "modificado@email.com");
+        //editor.putString("nombre", "Prueba");
+        //editor.commit();
         else if (id == R.id.language) {
 
             showDialog();
