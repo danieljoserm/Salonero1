0b0589806eba35cf1a17e59c14db73b08122b29b
danie <danieljoserm@gmail.com>
2017-06-26 13:08:34 -0600

    Previo agregar SimpleSectionedRecyclerViewAdapter

--- app/src/main/java/main/salonero1/Adapters/adaptermenuitem.java
+++ app/src/main/java/main/salonero1/Adapters/adaptermenuitem.java
@@ -56,47 +56,7 @@ public class adaptermenuitem extends RecyclerView.Adapter<adaptermenuitem.ViewHo
 
     adaptersubnombre adapter;
 
-    public Bitmap blurBitmap(Bitmap bitmap, Context context){
 
-//Let’s create an empty bitmap with the same size of the bitmap we want to blur
-        Bitmap outBitmap = Bitmap.createBitmap(bitmap.getWidth(),   bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-
-
-//Instantiate a new Renderscript
-        RenderScript rs = RenderScript.create(context);
-
-
-//Create an Intrinsic Blur Script using the Renderscript
-        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
-
-
-//Create the in/out Allocations with the Renderscript and the in/out bitmaps
-        Allocation allIn = Allocation.createFromBitmap(rs, bitmap);
-        Allocation allOut = Allocation.createFromBitmap(rs, outBitmap);
-
-
-//Set the radius of the blur
-        blurScript.setRadius(25.f);
-
-
-//Perform the Renderscript
-        blurScript.setInput(allIn);
-        blurScript.forEach(allOut);
-
-
-//Copy the final bitmap created by the out Allocation to the outBitmap
-        allOut.copyTo(outBitmap);
-
-
-//recycle the original bitmap
-        bitmap.recycle();
-
-
-//After finishing everything, we destroy the Renderscript.
-        rs.destroy();
-        return outBitmap;
-
-    }
 
 
 
@@ -142,7 +102,7 @@ public class adaptermenuitem extends RecyclerView.Adapter<adaptermenuitem.ViewHo
 
 
 
-            viewHolder.titulodescripcion.setText(menuitem.getNombremenuitem());
+        viewHolder.titulodescripcion.setText(menuitem.getNombremenuitem());
         viewHolder.nombremenuitem12.setText( menuitem.getNombremenuitem());
        viewHolder.precio.setText("Precio:" + Integer.toString(menuitem.getPrecio()));
 
